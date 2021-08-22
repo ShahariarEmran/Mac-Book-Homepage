6afe8a3989578be4bb013a0ae2af55ab62c7e48b
shahariaremran <emranshahariar@gmail.com>
2021-08-22 22:24:23 +0600

    first commit

---
+++ mac.js
@@ -0,0 +1,25 @@
+function allButton(){
+    let bestPrice = document.getElementById('')
+}
+
+document.getElementById('8gb').addEventListener('click', function(){
+    allButton('extra memory', 0);
+});
+document.getElementById('16gb').addEventListener('click', function(){
+    allButton('extra memory', 180);
+});
+document.getElementById('256gb').addEventListener('click', function(){
+    
+})
+document.getElementById('512gb').addEventListener('click', function(){
+
+})
+document.getElementById('1tb').addEventListener('click', function(){
+
+})
+document.getElementById('free-delivery').addEventListener('click', function(){
+
+})
+document.getElementById('paid-delivery').addEventListener('click', function(){
+
+})
